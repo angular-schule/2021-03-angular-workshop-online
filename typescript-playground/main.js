96295a3762d43eb6e6a468438b094b3dd88fa85e
Ferdinand Malcher <ferdinand@malcher.media>
2021-03-03 10:42:12 +0100

    Arrow Functions

--- typescript-playground/main.js
+++ typescript-playground/main.js
@@ -2,8 +2,19 @@ var Customer = /** @class */ (function () {
     function Customer(id) {
         this.id = id;
     }
+    Customer.prototype.fooBar = function (foo) {
+        var _this = this;
+        setTimeout(function () {
+            console.log('Die ID ist', _this.id);
+        }, 2000);
+        console.log(this.id);
+        return 'Hallo';
+    };
     return Customer;
 }());
-var myCustomer = new Customer(3);
-console.log(myCustomer);
+var myCustomer = new Customer(10);
+myCustomer.fooBar(5);
+setTimeout(function () {
+    console.log('MC', myCustomer);
+}, 2000);
 //# sourceMappingURL=main.js.map
--- typescript-playground/main.ts
+++ typescript-playground/main.ts
@@ -4,8 +4,32 @@ class Customer {
     constructor(id: number) {
         this.id = id;
     }
+
+    // Methode
+    fooBar(foo: number): string {
+        setTimeout(() => {
+            console.log('Die ID ist', this.id);
+        }, 2000);
+
+        console.log(this.id);
+
+        // Funktion
+        function bar() {}
+
+        return 'Hallo';
+    }
 }
 
 
-const myCustomer = new Customer(3);
-console.log(myCustomer);
+const myCustomer = new Customer(10);
+myCustomer.fooBar(5);
+
+// Funktion
+setTimeout(function() {
+    console.log('MC', myCustomer);
+}, 2000);
+
+// Funktionm
+function foo () {
+
+}
