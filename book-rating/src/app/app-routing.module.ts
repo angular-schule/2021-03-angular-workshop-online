51f935ca9df7174f0fa25497d0baed491e9e9ac8
Ferdinand Malcher <ferdinand@malcher.media>
2021-03-08 12:04:24 +0100

    Lazy Loading

--- book-rating/src/app/app-routing.module.ts
+++ book-rating/src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: '/books', pathMatch: 'full' },
+  {
+    path: 'books',
+    loadChildren: () => import('./books/books.module').then(m => m.BooksModule)
+  }
 ];
 
 @NgModule({
